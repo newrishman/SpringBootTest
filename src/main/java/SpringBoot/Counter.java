e506c0e578119c14c1f5775ef76ea4220fdca87b
newrishman <safarovrishat@gmail.com>
2019-06-16 23:42:44 +0300

    It's good cod

--- src/main/java/SpringBoot/Counter.java
+++ src/main/java/SpringBoot/Counter.java
@@ -2,18 +2,18 @@ package SpringBoot;
 
 import org.springframework.stereotype.Service;
 
-    @Service
-    public class Counter{
-        private int count = 0;
+@Service
+public class Counter {
+    private int count = 0;
 
-        public void intCount(){
-            count++;
-        }
-
-        public int getValue(){
-            return count;
-        }
+    public void intCount() {
+        count++;
     }
+
+    public int getValue() {
+        return count;
+    }
+}
 
 
 
