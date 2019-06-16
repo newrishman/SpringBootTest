ce2a28ba727e8950889fdbc7d7dd977f24936fe6
newrishman <safarovrishat@gmail.com>
2019-06-16 20:54:27 +0300

    I think it is not good

--- src/main/java/SpringBoot/GreetingController.java
+++ src/main/java/SpringBoot/GreetingController.java
@@ -1,21 +1,17 @@
 package SpringBoot;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
     @GetMapping("/0")
-    public int greeting(@RequestParam(name = "0", required = false) String name,
-                           Model model
-    ) {
-        model.addAttribute("name", name);
+    public int greeting() {
         return 0;
     }
 
 }
+
